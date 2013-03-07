10dc4e7c87ef6c393fb2e4c4ab18604331592887
zjor <zjor.se@gmail.com>
2013-03-07 15:33:28 +0400

    landing page: grammar fixes

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ $PLAY_COMPILER war ./ -o ../$TMP_FOLDER
 $PLAY_COMPILER clean
 
 cd ../$TMP_FOLDER
-zip -r root.war WEB-INF
-mv root.war ../
+zip -r instabank.war WEB-INF
+mv instabank.war ../
 cd ../
 rm -rf $TMP_FOLDER
